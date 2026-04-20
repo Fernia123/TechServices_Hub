c680ca2b1e0d072b1889cb825b3df2c065e49958
AndhreSoriano <daitiut@gmail.com>
2026-04-20 16:35:34 -0500

    Delete models directory

--- models/index.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('../config/database');
-const Usuario = require('./Usuario')(sequelize);
-const Servicio = require('./Servicio')(sequelize);
-
-// === ASOCIACIONES ===
-// (Aquí se pueden añadir asociaciones futuras)
-
-module.exports = { sequelize, Usuario, Servicio };
