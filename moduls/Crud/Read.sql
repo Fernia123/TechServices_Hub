3a723ba000d80520e48f2b8ba25fc2eb2d73e5e8
Joshue Jesús De la Cruz Moquillaza <158598516+Fernia123@users.noreply.github.com>
2026-04-20 11:19:24 -0500

    Delete moduls directory

--- moduls/Crud/Read.sql
+++
@@ -1,9 +0,0 @@
-const tickets = await Solicitud.findAll({
-  where: { usuario_id: 5, deleted_at: null }, // paranoid filtra automáticamente, pero por claridad:
-  include: [
-    { model: Usuario, as: 'cliente', attributes: ['nombre', 'email'] },
-    { model: Servicio, as: 'servicio', attributes: ['nombre', 'precio_base'] },
-    { model: Historial, as: 'historial', order: [['createdAt', 'DESC']] }
-  ],
-  order: [['createdAt', 'DESC']]
-});
